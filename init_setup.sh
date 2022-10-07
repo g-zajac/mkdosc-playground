2a764c8fb231a3f08ffad9fc829a2c3bc2559908
Grzegorz on Mac <grzegorz.zajac@me.com>
2022-10-07 23:36:08 +0200

    add echo with python ver variable

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "START"
+export _VERSION_=3.8
+echo [$(date)]: "creating environment with python ${_VERSION_}"
+conda create --prefix ./env python=${_VERSION_} -y
+echo [$(date)]: "activate environment"
 source activate ./env
-pip install -r requirments.txt
+echo [$(date)]: "install requirements"
+pip install -r requirements.txt
+echo [$(date)]: "END"
+
+# to remove everything -
+# rm -rf env/ .gitignore conda.yaml README.md .git/
